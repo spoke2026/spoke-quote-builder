40397947f25cbfe992baebc81c94d37952bb9a05
Edward <edward@spoke.nz>
2026-05-21 16:41:06 +1200

    Hardcode Supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://lehrjxiabetjrmmrytjd.supabase.co';
+const supabaseAnonKey = 'sb_publishable_0HWTwU3YR4QcGOXGhKbCBA_Uy4aS6Pi';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
